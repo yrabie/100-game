5e920daa0c568d75f7832f97e80c90e94ed468d7
yrabie <36334100+yrabie@users.noreply.github.com>
2018-02-12 11:15:56 +0200

    Add files via upload

---
+++ yara 7.py
@@ -0,0 +1,21 @@
+print('player1')
+print('player2')
+count1=0
+count2=0
+lst=[1,2,3,4,5,6,7,8,9,10]
+While count1<100:
+    print (lst)
+    print('player1 choose from lst')
+    print(l[i])
+    count1=count 1+lst[i]
+While count2<100:
+    print (lst)
+    print ('player2 choose from lst')
+    print(l[i])
+    count2=count2+lst[i]
+if count1==100 and count1>count2:
+    print ('player1 wins game')
+else:
+    if count2==100 and count2>count1:
+        print ('player2 wins game')
+        
